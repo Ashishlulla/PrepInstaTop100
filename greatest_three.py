706bb567c1665ef9484c271b453239d01228db60
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-10 08:16:35 +0530

    Add files via upload

---
+++ greatest_three.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the Number: "))
+num2 = int(input("Enter the Number: "))
+num3 = int(input("Enter the Number: "))
+
+if num1 > num2 > num3:
+    print(num1)
+elif num2 > num3 > num1:
+    print(num2)
+else:
+    print(num3)
+    
