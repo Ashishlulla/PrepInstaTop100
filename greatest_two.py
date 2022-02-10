c317d815c3a279cc6af206e251de63afd15afd6c
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-10 08:17:00 +0530

    Add files via upload

---
+++ greatest_two.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the Number: "))
+num2 = int(input("Enter the Number: "))
+if num2 > num1:
+    print("Num2 is Greater")
+elif num2 < num1:
+    print("Num is  Greater")
+else:
+    print("Both are equal")
